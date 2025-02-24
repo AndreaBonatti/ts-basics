912023ceea5ff68685e425dc96a865a9a555693d
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 16:39:35 +0100

    any type

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@
 let sales = 123_456_7890; // in ts we can divide large digits with _
 let course = "TypeScript";
 let is_published = true;
-let level; // type === any
+let level; // type === any, => we can change the type of value => bad
+level = 1;
+level = "a";
+
+function render(document: any) { // implict cast of any inside a function create a problem => we need to explicit add : any
+    console.log(document)
+}
