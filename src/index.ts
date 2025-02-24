1fc34d11bddc7b24eda6ff1349026e5288093fa2
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 16:46:19 +0100

    arrays

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-// let sales: number = 1234567890;
-// let sales: number = 123_456_7890; // in ts we can divide large digits with _
-// let course: string = "TypeScript";
-// let is_published: boolean = true;
-// ts known the value of a variable if it has a assigned value => we can remove the annotation in every let variable up here
-let sales = 123_456_7890; // in ts we can divide large digits with _
-let course = "TypeScript";
-let is_published = true;
-let level; // type === any, => we can change the type of value => bad
-level = 1;
-level = "a";
-
-function render(document: any) { // implict cast of any inside a function create a problem => we need to explicit add : any
-    console.log(document)
-}
+let numbers =[1, 2 ,'3']; // in js the arrays can contain elements of different type => more problems
+let numbersToo: number[] =[1, 2 ,3]; // we can add the annotation to avoid the problem
+let numberTooAgain = []; // array of any
+numberTooAgain[0] = 1;
+numberTooAgain[2] = "69";
+// to avoid the problem here we need to add again the annotation
+let numberTooAgainToo: number[] = [];
+numberTooAgainToo.forEach(n => n.toPrecision) // the annotation help with the suggestion that the program can give us
