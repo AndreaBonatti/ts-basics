708f4bc578d5ad64f7c92885b7aa4af65e726c87
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 17:04:32 +0100

    enums

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-let user: [number, string] = [1, "Mosh"];
-user.push(1); // break the code because is no more a tuple
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalCase
+// enum Size {Small, Medium, Large} // Default value of the first element of the enum is 0, if we want another value we need to explicitly assigned it
+// enum Size {Small="s", Medium="m", Large="l"}
+// enum Size {Small = 1, Medium, Large} // The other value are increased from the first value
+// let mySize: Size = Size.Medium
+// console.log(mySize);
+const enum Size {Small = 1, Medium, Large} // using a const with enum generate a more optimized code
+let mySize: Size = Size.Medium
+console.log(mySize);
+
