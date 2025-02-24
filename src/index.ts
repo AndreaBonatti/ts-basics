f8142ad09a3783cb4aad3e1de3f6a201e761668c
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 16:35:12 +0100

    built-in types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age)
+// let sales: number = 1234567890;
+// let sales: number = 123_456_7890; // in ts we can divide large digits with _
+// let course: string = "TypeScript";
+// let is_published: boolean = true;
+// ts known the value of a variable if it has a assigned value => we can remove the annotation in every let variable up here
+let sales = 123_456_7890; // in ts we can divide large digits with _
+let course = "TypeScript";
+let is_published = true;
+let level; // type === any
