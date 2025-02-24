a7e0ef12ecca931dc36a0d65c66e07536faf7a6e
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 16:55:34 +0100

    tuples

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-let numbers =[1, 2 ,'3']; // in js the arrays can contain elements of different type => more problems
-let numbersToo: number[] =[1, 2 ,3]; // we can add the annotation to avoid the problem
-let numberTooAgain = []; // array of any
-numberTooAgain[0] = 1;
-numberTooAgain[2] = "69";
-// to avoid the problem here we need to add again the annotation
-let numberTooAgainToo: number[] = [];
-numberTooAgainToo.forEach(n => n.toPrecision) // the annotation help with the suggestion that the program can give us
+let user: [number, string] = [1, "Mosh"];
+user.push(1); // break the code because is no more a tuple
