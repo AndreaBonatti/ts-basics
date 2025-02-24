87c424da025b1c8bda3b4a0bf205d37e963b0217
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 12:06:02 +0100

    variable ts vs js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.log("Hello World");
+var age = 20;
+// age = "20"; Non funziona in quanto ts a differenze di js è statically-typed: il tipo di variabile è conosciuto durante la compilazione del programma, e non può cambiare una volta che è stato assegnato
