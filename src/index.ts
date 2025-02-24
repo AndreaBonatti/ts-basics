01cc2fac9da9f430a6ab183203a9985b30b5d15e
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-24 15:50:53 +0100

    debug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-console.log("Hello World")
-
 let age: number = 20;
-// age = "20"; Non funziona in quanto ts a differenze di js è statically-typed: il tipo di variabile è conosciuto durante la compilazione del programma, e non può cambiare una volta che è stato assegnato
+if (age < 50)
+    age += 10;
+console.log(age)
