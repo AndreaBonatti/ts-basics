12a6df5730adbdbe262478b7a6aa0653af1ae198
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-25 09:30:02 +0100

    typealias

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-// let employee = {id: 1};
-// employee.name = "Mosh"; // in js we can add new properties to an existing object later != ts
-// let employee: {
-//     id: number,
-//     name: string
-// } = {id: 1, name :""};
-// 
-// let employeeToo: { // This is possible but a bad practice beacuse an employee without a name has no sense
-//     id: number,
-//     name?: string
-// } = {id: 1};
-
-// Read-only properties => readolny
+// If we wanna create another employee we need to repeat the employee structure
+// We wanna always use the DRY principle => DRY === DO NOT REPEAT YOURSELF
 let employee: {
     readonly id: number,
     name: string,
@@ -18,9 +7,21 @@ let employee: {
 } = {
     id: 1,
     name :" Mosh", 
-    // retire(date) {
-    //     console.log(date);
-    // }
+    retire: (date: Date) => {
+        console.log(date)
+    }
+};
+
+// => use of typealias
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+
+let employeeToo: Employee = {
+    id: 1,
+    name :" Mosh", 
     retire: (date: Date) => {
         console.log(date)
     }
