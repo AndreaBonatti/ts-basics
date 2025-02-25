b85e0cc17a0810d2f0948175dc629718ba594817
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-25 09:58:34 +0100

    optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
-function greet(name: string) {
-    console.log(name.toUpperCase());
+// type Customer = {
+//     birthday: Date
+// };
+
+// function getCustomer(id: number): Customer | null {
+//     return id === 0 ? null : {birthday: new Date()}
+// }
+// 
+// let customer = getCustomer(0);
+// // console.log(customer.birthday); errore per chè customer può essere null
+// // 1) 
+// if(customer !== null) 
+//     console.log(customer.birthday);
+
+
+// function getCustomer(id: number): Customer | null | undefined {
+//     return id === 0 ? null : {birthday: new Date()}
+// }
+// 
+// let customer = getCustomer(0);
+// // console.log(customer.birthday); errore per chè customer può essere null
+// // 1) 
+// if(customer !== null && customer !== undefined) 
+//     console.log(customer.birthday);
+// // 2) optional property access operator
+// console.log(customer?.birthday);
+
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : {birthday: new Date()}
 }
 
-greet(null); // crash on compilation, controllo da strictNullChecks in tsconfig.json
+let customer = getCustomer(0);
+if(customer !== null && customer !== undefined) 
+    console.log(customer.birthday);
+console.log(customer?.birthday?.getFullYear);
 
-function greetToo(name: string | null | undefined) {
-    if(name)
-        console.log(name.toUpperCase());
-    else
-        console.log("Hola!")
-}
+// optional element access operator
+// if (customers !== null && customers !== undefined)
+//  customers[0]
+// customers?.[0]
+
+// optional call
+let log: any = null;
+log?.("a");
